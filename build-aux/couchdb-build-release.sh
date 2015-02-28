bc0b07ab7034d6052ee642749007359f8836cf33
Robert Kowalski <rok@kowalski.gd>
2015-02-28 22:58:41 +0100

    wip

--- build-aux/couchdb-build-release.sh
+++ build-aux/couchdb-build-release.sh
@@ -5,46 +5,96 @@ RELDIR=apache-couchdb
 rm -rf $RELDIR
 mkdir $RELDIR
 
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+REPOS="b64url cassim couch_log config chttpd couch couch_index \
+couch_replicator couch_dbupdates couch_plugins couch_event couch_stats \
+docs ddoc_cache ets_lru fauxton global_changes ioq khash mem3 rexi \
+setup mango couchdb"
 
-# copy sources over
-git archive $CURRENT_BRANCH | tar -xC $RELDIR/
-mkdir $RELDIR/src
-cd src/
+CONTRIB_EMAIL_SED_COMMAND="s/^[[:blank:]]{5}[[:digit:]]+[[:blank:]]/ * /"
 
-for repo in *; do
-  cd $repo
-  mkdir ../../$RELDIR/src/$repo
-  # todo, make work for tags
-  git archive `git rev-parse --abbrev-ref HEAD` | tar -xC ../../$RELDIR/src/$repo/
-  cd ..
-done
+function get_contributors {
+  if [$! == "couchdb"]; then
 
-cd ..
 
-# update version
-# actual version detection TBD
-perl -pi -e 's/\{vsn, git\}/\{vsn, "version"\}/' $RELDIR/src/*/src/*.app.src
-
-cp install.mk $RELDIR/install.mk
+  else
+    cd src/$1
+    git shortlog -se HEAD \
+      | grep -v @apache.org \
+      | sed $SED_ERE_FLAG -e "$CONTRIB_EMAIL_SED_COMMAND"
+    cd ..
+    cd ..
+  fi
+}
 
 # create THANKS file
 if test -e .git; then
-    OS=`uname -s`
-    case "$OS" in
-    Linux|CYGWIN*) # GNU sed
-        SED_ERE_FLAG=-r
-    ;;
-    *) # BSD sed
-        SED_ERE_FLAG=-E
-    ;;
-    esac
+  OS=`uname -s`
+  case "$OS" in
+  Linux|CYGWIN*) # GNU sed
+      SED_ERE_FLAG=-r
+  ;;
+  *) # BSD sed
+      SED_ERE_FLAG=-E
+  ;;
+  esac
 
-    sed -e "/^#.*/d" THANKS.in > $RELDIR/THANKS
-    CONTRIB_EMAIL_SED_COMMAND="s/^[[:blank:]]{5}[[:digit:]]+[[:blank:]]/ * /"
-    git shortlog -se 6c976bd..HEAD \
-        | grep -v @apache.org \
-        | sed $SED_ERE_FLAG -e "$CONTRIB_EMAIL_SED_COMMAND" >> $RELDIR/THANKS
-    echo "" >> $RELDIR/THANKS # simplest portable newline
-    echo "For a list of authors see the \`AUTHORS\` file." >> $RELDIR/THANKS
+  sed -e "/^#.*/d" THANKS.in > $RELDIR/THANKS
+
+  # unix pipes!
+  # get all single commits of persons over the repos and count them
+  { for i in $REPOS; do
+      get_contributors $i
+    done | awk '
+    BEGIN {
+
+    }
+    {
+      all[$NF] = 1;
+      if ($1 ~ /\*/) {
+        single[length(all)] = $0;
+      }
+    }
+    END {
+      for (i in single) {
+        print substr(single[i],3)
+      }
+
+    }' | sort | uniq -c;
+  # get multiple commits from git shortlog that already got numbers
+    for i in $REPOS; do
+      get_contributors $i
+    done | awk '
+    BEGIN {
+    }
+    {
+      if ($1 ~ /[0-9]/) {
+        count = $1
+        $1=""
+        committer[$0] = committer[$0] + count;
+      }
+    }
+    END {
+      for (i in committer) {
+        print committer[i], i
+      }
+    }'
+  # pipe them into an awk which sums them all up
+  } | awk '
+    BEGIN {
+
+    }
+    {
+      count = $1
+      $1=""
+      committer[$0] = committer[$0] + count;
+    }
+    END {
+      for (i in committer) {
+        print committer[i], i
+      }
+    }' > $RELDIR/THANKS
+
+    echo "" >> $RELDIR/THANKS
 fi
+
+
